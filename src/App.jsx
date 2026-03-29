90caab933d8426120deeb858a2a94438378fce3d
samalek9391 <sa.malek9391@gmail.com>
2026-03-29 21:44:04 +0600

    Project is initaited

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import NavBar from './assets/Components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+      
+        <NavBar/>
+          
+    </>
+  )
+}
+
+export default App
